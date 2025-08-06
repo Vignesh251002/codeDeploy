98cb046afea82524a4d7185744f802521e48a177
Vignesh251002 <vignesh.i@meyicloud.com>
2025-08-06 17:58:23 +0530

    commit

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda via CodeDeploy!"),
+    body: JSON.stringify("Hello from Lambda via CodeDeploy! vignesh"),
   };
 };
